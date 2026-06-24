71f1266356791ae479094d3ee647c5702236557b
Guillaume <guillaume-duclos@hotmail.fr>
2026-06-24 14:32:19 -0400

    WIP

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./navigation/Navigation";
@@ -9,12 +10,23 @@ import { ThemeProvider } from "./providers/ThemeContext";
 import "./translations/translation";
 
 export default function App() {
-  useFonts({
+  const [fontsLoaded] = useFonts({
     Rubik: require("./assets/fonts/Rubik/RubikVariable.ttf"),
     RubikItalic: require("./assets/fonts/Rubik/RubikItalicVariable.ttf"),
     GoogleSansCode: require("./assets/fonts/GoogleSansCode/GoogleSansCodeVariable.ttf"),
     GoogleSansCodeItalic: require("./assets/fonts/GoogleSansCode/GoogleSansCodeItalicVariable.ttf"),
   });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      console.log("Hide SplashScreen");
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <ThemeProvider>
